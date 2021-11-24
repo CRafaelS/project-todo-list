0bdcaf5abf67799782d770e62898ef0ff44702df
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-23 21:24:59 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const button = document.getElementById('criar-tarefa');
-button.addEventListener('click', Add);
+
 // https://pt.stackoverflow.com/questions/21860/como-pegar-input-usando-html-e-javascript
+
 function Add() {
-    const taskList = document.getElementById('lista-tarefas');
-    const task = document.getElementById('texto-tarefa');
-    const createList = document.createElement('li');
-    
-    createList.innerHTML = task.value;
-    taskList.appendChild(createList);
-    task.value = '';
+  const taskList = document.getElementById('lista-tarefas');
+  const task = document.getElementById('texto-tarefa');
+  const createList = document.createElement('li');
+
+  createList.innerHTML = task.value;
+  taskList.appendChild(createList);
+  createList.classList.add('Lista');
+  task.value = '';
 }
+button.addEventListener('click', Add);
+
+const taskList = document.getElementById('lista-tarefas');
+
+function changeBG(event) {
+  event.target.classList.add('select');
+}
+taskList.addEventListener('click', changeBG);

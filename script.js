7bc8eac265aec537e2a85f58a6e2ff5ddff8d7d4
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-24 16:45:46 -0300

    Requisito 10 e 11

--- script.js
+++ script.js
@@ -31,16 +31,38 @@ taskList.addEventListener('click', changeBG);
 
 // https://www.w3schools.com/jsref/event_ondblclick.asp
 
-taskList.addEventListener('dblclick', completDpClick);
-
 function completDpClick(e) {
   if (e.target.classList.contains('select')) {
     e.target.classList.remove('select');
-    if(e.target.classList.contains('completed')) {
+    if (e.target.classList.contains('completed')) {
       e.target.classList.remove('completed');
-    }
-    else {
+    } else {
       e.target.classList.add('completed');
     }
   }
 }
+
+taskList.addEventListener('dblclick', completDpClick);
+
+const clearList = document.getElementById('apaga-tudo');
+
+function clearAll() {
+  // alert ('Toda Lista será apagada');
+  const clear = document.getElementsByTagName('li');
+  for (let i = clear.length - 1; i >= 0; i -= 1) {
+    clear[i].remove('Lista');
+  }
+}
+clearList.addEventListener('click', clearAll);
+
+const cleanFinished = document.getElementById('remover-finalizados');
+
+function finished() {
+  const readList = document.getElementsByTagName('li');
+  for (let i = readList.length - 1; i >= 0; i -= 1) {
+    if (readList[i].classList.contains('completed')) {
+      readList[i].remove('completed');
+    }
+  }
+}
+cleanFinished.addEventListener('click', finished);
